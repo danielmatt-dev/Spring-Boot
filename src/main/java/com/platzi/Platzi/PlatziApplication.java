30dc03f77bfe07271d2cf5986710b833c35fa896
danielmatt-dev <danielmatt1701@gmail.com>
2024-02-27 09:54:41 -0600

    Inicio del proyecto

---
+++ src/main/java/com/platzi/Platzi/PlatziApplication.java
@@ -0,0 +1,14 @@
+package com.platzi.Platzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziApplication.class, args);
+		System.out.println("Hola Mundo");
+	}
+
+}
